818bcfb3f994a9d6bf430c2e3387ec0385206be7
PatMahaney <patrickmahaney10@gmail.com>
2023-05-24 22:52:28 -0400

    Sally

---
+++ Sally.cpp
@@ -0,0 +1,40 @@
+//
+//
+
+#include <iostream>
+using namespace std;
+void instructions()
+{
+	cout << "How many times you want to bring Sally up and Down?:";
+	return;
+}
+void getInput(int &ans)
+{
+	cin >> ans;
+}
+void lyrics(int ans)
+{
+	for (int i = 0; i < ans; i++)
+	{
+		cout << "Green Sally Up\nGreen Sally Down\nLift and Squat, got to tear the ground!\n\n" << endl;
+	}
+	return;
+}
+int main()
+{
+	int answer = 0;
+	instructions();
+	getInput(answer);
+	lyrics(answer);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
